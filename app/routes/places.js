18c97c768accb4cd7677c14490d0d1102613be7c
alexeibelozertsev <belozertsev.alexei@gmail.com>
2023-10-18 16:36:58 +0200

    fix: get changed to post method

--- app/routes/places.js
+++ app/routes/places.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
 const placesController = require("../controllers/index");
 
-const placesRoute = router.get("/places", placesController.addPlaces);
+const placesRoute = router.post("/places", placesController.addPlaces);
 module.exports = placesRoute;
