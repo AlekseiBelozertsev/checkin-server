9a848a29c8d5b34be2625f2c3dafdadc9198eed4
alexeibelozertsev <belozertsev.alexei@gmail.com>
2023-10-18 16:37:33 +0200

    fix: places controller updated with props

--- app/controllers/addPlace.js
+++ app/controllers/addPlace.js
@@ -2,18 +2,18 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 // Set up mock data and later connect it to the client
 const placesController = async (req, res) => {
+  const receivedData = req.body;
   const place = await prisma.place.create({
     // here data must be switched to real data from client. "data: req.body"
     data: {
-      id: 4,
-      name: "Something",
-      address: "Somewhere",
-      country: "Lalaland",
-      coordintes: ["-12.556", "54.6666"],
-      createdAt: new Date().toISOString(),
+      id: receivedData.id,
+      name: receivedData.name,
+      address: receivedData.address,
+      country: receivedData.country,
+      coordintes: receivedData.coordintes,
+      createdAt: receivedData.createdAt,
     },
   });
-  res.send(place);
 };
 
 module.exports = placesController;
