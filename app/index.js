522c3a9657803b1971354feb029626ca779e0c58
alexeibelozertsev <belozertsev.alexei@gmail.com>
2023-10-19 10:23:41 +0200

    feat: body-parser installed

--- app/index.js
+++ app/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const bodyParser = require("body-parser")
 const cors = require("cors");
 const places = require("./routes/places");
-
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
 app.use(places);
 module.exports = app;
