5b8637df628c4f2e4e51fb424941063b6ff2e18b
alexeibelozertsev <belozertsev.alexei@gmail.com>
2023-10-30 12:43:07 +0100

    fix: typo fixed

--- app/controllers/addPlace.js
+++ app/controllers/addPlace.js
@@ -17,7 +17,7 @@ const addPlaceController = async (req, res) => {
       name: receivedData.name,
       address: receivedData.address,
       country: getCountry(receivedData.country),
-      coordinates: receivedData.coordintes,
+      coordinates: receivedData.coordinates,
       createdAt: receivedData.createdAt,
     },
   });
